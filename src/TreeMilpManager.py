758bad734febfcf3786e90aaab30025da7789f93
Awa Khouna <awa.khouna@polytechnique.edu>
2024-10-11 11:20:01 -0400

    Remove prints

--- src/TreeMilpManager.py
+++ src/TreeMilpManager.py
@@ -186,14 +186,14 @@ class TreeInMilpManager:
 
     def addContinuousVariablesConsistencyConstraints(self):
         if self.constraintsType == TreeConstraintsType.ExtendedFormulation:
-            print('Warning: this tree constraint type should be avoided,'
-                  ' use TreeConstraintsType.LinearCombinationOfPlanes'
-                  ' instead.')
+            #print('Warning: this tree constraint type should be avoided,'
+            #      ' use TreeConstraintsType.LinearCombinationOfPlanes'
+            #      ' instead.')
             self.addTreeExtendedFormulationVariablesAndConstraints()
         elif self.constraintsType == TreeConstraintsType.BigM:
-            print('Warning: this tree constraint type should be avoided,'
-                  ' use TreeConstraintsType.LinearCombinationOfPlanes'
-                  ' instead.')
+            #print('Warning: this tree constraint type should be avoided,'
+            #      ' use TreeConstraintsType.LinearCombinationOfPlanes'
+            #      ' instead.')
             self.addTreeBigMConstraints()
         elif (self.constraintsType
               == TreeConstraintsType.LinearCombinationOfPlanes):
